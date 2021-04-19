6fb5eee03b1911650eaac64b8a0ec1316ddc3cfc
dan <a2990@n-koei.co.jp>
2021-04-20 04:23:19 +0900

    upgrade

--- useGdal/ex2_useGdal.py
+++ useGdal/ex2_useGdal.py
@@ -5,33 +5,46 @@ import sys
 with open('enableGdal.py') as f:
     exec(f.read()) # import ogr
 
-if os.path.exists('poly4.json'):
-    driver = ogr.GetDriverByName('GeoJSON')
-    dataSource = driver.Open('poly4.json')
-else:
-    sys.exit('poly4.json not found. run ex1_useGdal first')
+def info(filename):
 
-layer = dataSource.GetLayer()
-for i, feature in enumerate(layer):
-    geometry     = feature.GetGeometryRef()
-    geometryType = geometry.GetGeometryType()
-    geometryName = ogr.GeometryTypeToName(geometryType)
-    print(' feature[{}]'.format(i))
-    print( '\ttype : {}'.format(geometryName))
-    if geometryName == 'Polygon':
-        geom_outer = ogr.ForceToLineString(geometry)
-        for j in range(geom_outer.GetPointCount()):
-            point = geom_outer.GetPoint(j)
+    drivers = {
+        '.geojson': 'GeoJSON',
+        '.json':    'GeoJSON',
+        '.shp':     'ESRI ShapeFile',
+        '.kml':     'KML',
+        '.sqlite':  'SQLite',
+        '.gpkg':    'GPKG'
+    }
+
+    if os.path.exists(filename):
+        ext = os.path.splitext(filename)[1].lower()
+        if ext in drivers:
+            driver = ogr.GetDriverByName(drivers[ext])
+        else:
+            sys.exit("sorry, '.{}' not support".format(ext))
+        dataSource = driver.Open(filename)
+    else:
+        sys.exit('{} not found.'.format(filename))
+
+    layer = dataSource.GetLayer()
+    for i, feature in enumerate(layer):
+        geometry     = feature.GetGeometryRef()
+        geometryType = geometry.GetGeometryType()
+        geometryName = ogr.GeometryTypeToName(geometryType)
+        print(' feature[{}]'.format(i))
+        print( '\ttype : {}'.format(geometryName))
+        if   geometryName == 'Polygon':
+            geom = ogr.ForceToLineString(geometry)
+        elif geometryName == 'Point':
+            geom = geometry
+        else:
+            continue
+        for j in range(geom.GetPointCount()):
+            point = geom.GetPoint(j)
             x, y  = point[0], point[1]
             print('\tcodinates[{}] : \t{}\t{}'.format(j, x, y))
 
-''' above scripts same below
-import json
+if __name__ == '__main__':
 
-with open('poly4.json', encoding='utf-8') as f:
-    data_dict = json.load(f)
-
-for i, point in enumerate(data_dict['coordinates'][0]):
-    x, y = point[0], point[1]
-    print('\tcodinates[{}] : \t{}\t{}'.format(j, x, y))
-'''
+    if len(sys.argv) == 2:
+        info(sys.argv[1])
