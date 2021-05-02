f1ae479169f8a053d06afdb70d263c4e6c8b6f02
dan <a2990@n-koei.co.jp>
2021-05-03 02:27:22 +0900

    upgrade photoView.py

--- useCase_3/photoView.py
+++ useCase_3/photoView.py
@@ -71,7 +71,7 @@ def main(dir = '.'):
 			feature = {
 				'type': 'Feature',
 			    'geometry': {'type': 'Point', 'coordinates': info['lnglat']},
-				'properties': {'src': src, 'time': info['time']}
+				'properties': {'img': src, 'caption': '', 'time': info['time']}
 			}
 			features.append(feature)
 
@@ -81,10 +81,11 @@ def main(dir = '.'):
 	geojson['bbox'] = boundBox(points)
 
 	with open(dir + '/clickMe.html', 'w', encoding = 'utf-8') as f:
-		s  = '<!DOCTYPE html><html><head><meta charset="UTF-8"><title>photoView</title><link rel="stylesheet"href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css"/><script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js"></script><style>body {padding: 0; margin: 0}html, body, #map {height: 100%; width: 100%;}  textarea {font-weight: bold;}.leaflet-popup-content {width: 200px;}.thumbnail {max-width:200px; max-height:200px}</style></head><body><div id="map"></div><script>var map=L.map("map");L.tileLayer("https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png").addTo(map);'
-		s += 'var geojson=' + json.dumps(geojson) + ';'
-		s += 'var bbox=geojson.bbox;var southWest=[bbox[0][1],bbox[0][0]];var northEast=[bbox[1][1],bbox[1][0]];map.fitBounds([southWest,northEast]);L.geoJSON(geojson).bindPopup((layer)=>{var prop=layer.feature.properties;var src=prop.src;var html="<p><b>"+prop.src+"</b><br>"+prop.time+"</p>";html+="<a href=\'"+src+"\' target=\'_blank\' download>";html+="<img class=\'thumbnail\' src=\'"+ src+"\'></a>";return html;}).addTo(map);</script></body></html>'
+		s  = '<!DOCTYPE html><html><head><meta charset="UTF-8"><title>View Photos on Map</title><link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"/><script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script><script src="exif.js"></script><style>body{padding: 0; margin: 0}html, body, #map{height: 100%; width: 100%}.caption{font-weight: bold}.leaflet-popup-content{width: 200px;}.thumbnail{max-width:200px; max-height:200px}</style></head><body><div style="text-align:right;"><input id="save" type="button" value="saveCange"></div><div id="map"></div><script>var btn=document.getElementById("save");btn.disabled=true;btn.addEventListener("click", ()=>{var contents=\'var geojson=\'; contents +=JSON.stringify(geojson, "", 2);var blob=new Blob([contents],{type: "text.plain"});var a=document.createElement("a");document.body.appendChild(a);a.href=URL.createObjectURL(blob);a.download="exif.js";a.style="display: none";a.click();URL.revokeObjectURL(a.href);btn.disabled=true;});var map=L.map(\'map\');L.tileLayer(\'https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png\',{attribution: "<a href=\'https://maps.gsi.go.jp/development/ichiran.html\' target=\'_blank\'>地理院タイル</a>"}).addTo(map);bbox=geojson.bbox;southWest=[bbox[0][1], bbox[0][0]];northEast=[bbox[1][1], bbox[1][0]];map.fitBounds([southWest, northEast]);L.geoJSON(geojson) .bindPopup((layer)=>{var prop=layer.feature.properties; var img=prop.img;var html=\'<input type="text" id="caption" value="\' + prop.caption + \'" class="caption" placeholder="--caption--">\';html +="<p>" + prop.time + "</p>"; html +="<a href=\'" + img + "\' target=\'_blank\' download>"; html +="<img class=\'thumbnail\' src=\'" + img + "\'></a>"; return html;}) .on(\'popupclose\', function(ev){var elem=document.getElementById("caption"); if(elem){var caption=elem.value; var properties=ev.layer.feature.properties; if(properties.caption !=caption){properties.caption=caption; btn.disabled=false;}}}) .addTo(map);</script></body></html>'
 		f.write(s)
+
+	with open(dir + '/exif.js', 'w', encoding = 'utf-8') as f:
+		f.write('var geojson=' + json.dumps(geojson, indent=2) + ';')
 
 if __name__ == '__main__':
 
