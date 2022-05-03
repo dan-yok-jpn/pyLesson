8ba14bfd012a9b474890f360e4f5e1ee029578dc
dan_yok <dan_yok@outlook.jp>
2022-05-03 19:04:38 +0900

    upgrade

--- useCase_2/clipbd.py
+++ useCase_2/clipbd.py
@@ -3,61 +3,84 @@ import sys
 import io
 import pyperclip
 
-def copy():
-    pyperclip.copy(sys.stdin.read())
-    exit()
+COPY  = True
+PASTE = False
 
-def paste():
-    buf = pyperclip.paste()
-    if utf:
-        sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+def usage():
+
+    print('''
+Usage:
+    py {} [Options]
+Options:
+    -c    : copy to clipboad (default)
+    -p    : print clipboad
+    -tsv  : replace comma to tab
+    -csv  : replace tab to comma
+    -utf  : encoding utf-8
+    -sjis : encoding shift-jis (default)
+    -h    : show this help
+'''.format(os.path.basename(__file__)),
+    file = sys.stderr)
+
+def copy(csv, tsv, encoding):
+
+    sys.stdin = io.TextIOWrapper(sys.stdin.buffer, encoding = encoding)
+
+    buf = sys.stdin.read()
     if sjis:
-        sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='shift-jis')
         buf = buf.replace("\x80", "")
+
     if csv:
-        sys.stdout.write(buf.replace("\r", "").replace("\t", ","))
+        pyperclip.copy(buf.replace("\t", ","))
     elif tsv:
-        sys.stdout.write(buf.replace("\r", "").replace(",", "\t"))
+        pyperclip.copy(buf.replace(",", "\t"))
     else:
-        sys.stdout.write(buf.replace("\r", ""))
+        pyperclip.copy(buf)
+
+def paste(csv, tsv, utf, sjis):
+
+    buf = pyperclip.paste().replace("\r", "")
+
+    if utf:
+        sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8")
+    elif sjis:
+        sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="shift-jis")
+        buf = buf.replace("\x80", "")
+
+    if csv:
+        sys.stdout.write(buf.replace("\t", ","))
+    elif tsv:
+        sys.stdout.write(buf.replace(",", "\t"))
+    else:
+        sys.stdout.write(buf)
 
 if __name__ == "__main__":
 
-    global csv, tsv, utf, sjis
-    csv = tsv = utf = sjis = False
+    mode = COPY
+    csv, tsv  = False, False
+    sjis, utf = True,  False
 
-    if len(sys.argv) == 1:
-        copy()
+    for arg in sys.argv[1:]:
+        if arg == "-c":
+            mode = COPY
+        elif arg == "-p":
+            mode = PASTE
+        elif arg == "-tsv":
+            tsv = True
+        elif arg == "-csv":
+            csv = True
+        elif arg == "-utf":
+            sjis, utf = False, True
+        elif arg == "-sjis":
+            sjis, utf = True,  False
+        elif arg ==  "-h":
+            usage()
+            sys.exit()
+        else:
+            usage()
+            sys.exit(f'\n ERROR !!! {arg} no such option.')
+
+    if mode == COPY:
+        copy(csv, tsv, "utf-8" if utf else "shift-jis")
     else:
-        for arg in sys.argv[1:]:
-            if arg == "-c":
-                copy()
-            elif arg == "-p":
-                pass
-            elif arg == "-tsv":
-                tsv = True
-            elif arg == "-csv":
-                csv = True
-            elif arg == "-utf":
-                utf = True
-            elif arg == "-sjis" and not utf:
-                sjis = True
-            elif arg == "-h" or arg == "--help":
-                msg = '''
-Usage:
-  py {} [Options]
-
-Options:
- -c : copy to clipboad (default)
- -p : print clipboad
- -tsv : translate comma to tab, use with -p
- -csv : translate tab to comma, use with -p
- -utf : use code page utf-8, use with -p
- -sjis : use code page shift-jis, use with -p
- -h, --help : show this help '''.format(os.path.basename(__file__))
-                sys.exit(msg)
-            else:
-                sys.exit(f'''
- {arg} no such option.''')
-
-        paste()
+        paste(csv, tsv, utf, sjis)
